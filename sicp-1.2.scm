5dfe242590e16a009b231275d3331dc8d1049976
Pushpankar <pushpankark@gmail.com>
2020-05-18 22:31:39 +0530

    Practice question

--- sicp-1.2.scm
+++ sicp-1.2.scm
@@ -18,3 +18,43 @@
   (cons (cons 1 (moving-sum xs)) 1))
 
 (row '(1 3 3 1))
+;;1.17
+;;
+;;
+(define (fast-exp x n)
+  (cond
+   ((= n 0) 1)
+   ((even? n)
+    (square (fast-exp x (/ n 2))))
+   (else (* x (fast-exp x (- n 1))))))
+(fast-exp 4 28)
+;; x^n = (x^2)^(n/2) even
+;; x^n = x* x^(n-1) = x * (x^2)^((n-1)/2) odd
+(define (fast-exp-iter x n a)
+  (cond
+   ((= n 0) a)
+   ((even? n) (fast-exp-iter (square x) (/ n 2) a))
+   (else (fast-exp-iter x (- n 1) (* a x)))))
+
+(fast-exp-iter 4 28 1)
+
+;; 1.17
+;; a * b = (a + a + ... ntimes) = (a * b/2) + (a * b/2)
+;; a * b = a * (b-1) + a
+(define (fast-mul a b)
+  (cond
+   ((= b 1) a)
+   ((even? b) (double (fast-mul a (/ b 2))))
+   (else (+ a (fast-mul a (- b 1) )))))
+
+(define (double x)
+  (+ x x))
+(fast-mul 3 8)
+
+;; gcd
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(gcd 101011019283782 573)
---
+++ sicp-1.3.scm
@@ -0,0 +1,76 @@
+(define (summations f next start end)
+  (cond
+   ((> start end) 0)
+   (else (+ (f start) (summations f next (next start) end)))))
+
+(define (identity x)
+  x)
+(summations identity (lambda (x) (+ 1 x)) 1 10)
+
+;;
+(define (summations-iterative f next start end)
+  (define (iter-sum index total)
+    (cond
+    ((> index end) total)
+    (else (iter-sum (next index) (+ (f index) total)))))
+  (iter-sum start 0))
+
+(define (increment x)
+  (+ 1 x))
+(summations-iterative square increment 1 10)
+(increment 1)
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (summations f add-dx a b) dx))
+
+(integral cube 0 1 0.0001)
+
+;; 1.31
+(define (product term next start end)
+  (cond
+   ((> start end) 1)
+   (else (*
+          (term start)
+          (product term next (next start) end)))))
+
+(product square increment 1 3)
+(define (factorial n)
+  (product identity increment 1 n))
+(factorial 5)
+
+(define (product-iterative term next start end)
+  (define (iter index aggregate)
+    (cond
+     ((> index end) aggregate)
+     (else (iter (next index) (* index aggregate)))))
+  (iter start 1))
+
+(product-iterative identity increment 1 5)
+
+(define (accumulate combine null-value term a next b)
+  (cond
+   ((> a b) null-value)
+   (else (combine (term a) (accumulate combine null-value term (next a) next b)))))
+
+(define (add a b)
+  (+ a b))
+
+(accumulate (lambda (x y) (+ x y)) 0 square 1 increment 10)
+
+(define (accumulate-iter combine null-value term a next b)
+  (define (iter index agg)
+    (cond
+     ((> index b) agg)
+     (else (iter (next index) (combine (term index) agg)))))
+  (iter (next (next a)) (combine (term a) (term (next a)))))
+
+(accumulate-iter (lambda (x y) (+ x y)) 0 square 1 increment 10)
+
+(define (accumulate-filter combine null-value term predicate-fun a next b)
+  (cond
+   ((> a b) null-value)
+   ((predicate-fun (term a)) (combine (term a) (accumulate combine null-value term (next a) next b)))
+   (else (accumulate combine null-value term predicate-fun (next a) next b))))
+
+(accumulate-iter (lambda (x y) (+ x y)) 0 identity even? 1 increment 10)
