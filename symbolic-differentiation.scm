3c34de3253cc9e7e0bac0b040214e56b9894c48a
Pushpankar <pushpankark@gmail.com>
2020-06-10 21:08:47 +0530

    Delete procedure

--- symbolic-differentiation.scm
+++ symbolic-differentiation.scm
@@ -108,12 +108,3 @@
 (deriv-exp (make-exponentiation 'u 5) 'u)
 
 
-;; Generic ops
-(define (deriv exp var)
-  (cond
-   ((number? exp) 0)
-   ((variable? exp) (if (same-variable? exp var) 1 0))
-   (else ((get 'deriv (operator exp)) (operands exp) var))))
-
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
