b748edbf51b6a8558f9d88703cb6950273fba86f
Pushpankar <pushpankark@gmail.com>
2020-06-17 22:39:27 +0530

    Solve 3.2

--- chapter3.scm
+++ chapter3.scm
@@ -1,3 +1,4 @@
+;; 3.1
 (define (make-accumulator init)
   (define (increment value)
     (set! init (+ init value))
@@ -6,3 +7,18 @@
 (define A (make-accumulator 5))
 (A 5)
 (A 7)
+
+;; 3.2
+(define (make-monitered f)
+  (let ((counter 0))
+    (define (dispatch message)
+      (cond
+       ((eq? message 'how-many-calls?) counter)
+       ((eq? message 'reset-counter) (begin (set! counter 0) counter))
+       (else (begin (set! counter (+ 1 counter)) (f message)))))
+    dispatch))
+
+(define ms (make-monitered sqrt))
+(ms 5)
+(ms 'how-many-calls?)
+(ms 'reset-counter)
